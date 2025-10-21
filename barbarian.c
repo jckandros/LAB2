a15c60e8f7765e04344cbc0806f6605e847d51b7
Jack Andros <jckandros44@gmail.com>
2025-10-21 15:15:42 -0700

    barbarian.c start

---
+++ barbarian.c
@@ -0,0 +1,38 @@
+/* barbarian.c
+   Responds to DUNGEON_SIGNAL by copying enemy.health -> barbarian.attack,
+   then waits SECONDS_TO_ATTACK and leaves the value (Dungeon checks for success).
+   Responds to SEMAPHORE_SIGNAL by waiting on one of the levers to "hold" the door.
+*/
+
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include "dungeon_info.h"
+#include "dungeon_settings.h"
+#include <stdatomic.h>
+
+static volatile sig_atomic_t dungeon_signal_received = 0;
+static volatile sig_atomic_t sem_signal_received = 0;
+
+void handler_dungeon(int sig) { dungeon_signal_received = 1; }
+void handler_semaphore(int sig) { sem_signal_received = 1; }
+
+int main(void) {
+    /* Setup signal handlers */
+    struct sigaction sa;
+    sa.sa_handler = handler_dungeon;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sigaction(DUNGEON_SIGNAL, &sa, NULL);
+
+    struct sigaction sa2;
+    sa2.sa_handler = handler_semaphore;
+    sigemptyset(&sa2.sa_mask);
+    sa2.sa_flags = SA_RESTART;
+    sigaction(SEMAPHORE_SIGNAL, &sa2, NULL);
+
